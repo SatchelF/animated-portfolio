41d3eb2ac253e2c78e86b26650c5433eaef5d411
SatchelF <satchcollege@gmail.com>
2024-01-22 01:06:15 -0700

    Finished Portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import "./portfolio.scss"
 import {useRef} from "react"
-import {motion, useScroll, useSpring} from "framer-motion"
+import {motion, useScroll, useSpring, useTransform} from "framer-motion"
 const items = [
     {
         id:1,
@@ -28,37 +28,57 @@ const items = [
 
 ];
 
-const Single = ({item}) => {
+const Single = ({ item }) => {
+    const ref = useRef();
+  
+    const { scrollYProgress } = useScroll({
+      target: ref,
+    });
+  
+    const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+  
     return (
-        <section>
-            {item.title}
-        </section>
-    )
-
-}
-
-const Portfolio = () => {
-
-    const ref = useRef()
-
-    const {scrollYProgress} = useScroll({target:ref,offset:["end end","start start"],});
-
-    const scaleX = useSpring(scrollYProgress,{
-        stiffness:100,
-        damping: 30,
-    })
-    return <div className="portfolio" ref={ref}>
-        <div className="progress">
-        <h1>Featured Works</h1>
-        <motion.div style={{scaleX}}className="progressBar"></motion.div>
+      <section >
+        <div className="container">
+          <div className="wrapper">
+            <div className="imageContainer" ref={ref}>
+              <img src={item.img} alt="" />
+            </div>
+            <motion.div className="textContainer" style={{y}}>
+              <h2>{item.title}</h2>
+              <p>{item.desc}</p>
+              <button>See Demo</button>
+            </motion.div>
+          </div>
         </div>
-      {items.map(item=>(
-        <Single item={item} key={item.id}/>
-      ))}
-    </div>;
+      </section>
+    );
   };
   
+  const Portfolio = () => {
+    const ref = useRef();
   
+    const { scrollYProgress } = useScroll({
+      target: ref,
+      offset: ["end end", "start start"],
+    });
+  
+    const scaleX = useSpring(scrollYProgress, {
+      stiffness: 100,
+      damping: 30,
+    });
+  
+    return (
+      <div className="portfolio" ref={ref}>
+        <div className="progress">
+          <h1>Featured Works</h1>
+          <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+        </div>
+        {items.map((item) => (
+          <Single item={item} key={item.id} />
+        ))}
+      </div>
+    );
+  };
   
   export default Portfolio;
-  
